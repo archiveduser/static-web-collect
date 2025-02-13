5a56e6d105365a9fcf6c16f622ec9d3a4855419e
Guest <24353907+archiveduser@users.noreply.github.com>
2025-02-13 11:56:11 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,10 +5,6 @@ shopt -u dotglob
 git clone https://github.com/fanmingming/live.git ./repo/fanmingming-live --depth=1
 rsync -a --include='tv/***' --include='e.xml' --exclude='*' ./repo/fanmingming-live/ ./dist/fanmingming-live/
 
-# https://github.com/novnc/noVNC.git
-git clone https://github.com/novnc/noVNC.git ./repo/novnc --depth=1
-rsync -a --exclude='.*' --exclude='utils' --exclude='tests' --exclude='snap' --exclude='docs' ./repo/novnc/ ./dist/novnc/
-
 # stats
 echo "Size: $(du -sh ./dist)"
 echo "Count: $(tree ./dist |wc -l)"
